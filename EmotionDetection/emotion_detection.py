37fbb7f8c0bc5b562cbd47b892dd6fb50e589234
nikhilbawane <nikhilbawane03@gmail.com>
2024-08-12 03:11:51 -0400

    Incorporate Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -10,6 +10,16 @@ def emotion_detector(text_to_analyze: str):
 
     response = requests.post(url, json = myobj, headers = header)
 
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
     res = json.loads(response.text)
 
     prediction = res["emotionPredictions"][0]
--- server.py
+++ server.py
@@ -14,6 +14,9 @@ def get_emotion():
 
     res = emotion_detector(text_to_analyze)
 
+    if res["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
     return "For the given statement, the system response is" \
     f" 'anger': {res['anger']}," \
     f" 'disgust': {res['disgust']}," \
